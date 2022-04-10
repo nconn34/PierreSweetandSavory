9cb4c293571268d6152bdf5364d4025be89b33c8
Nathan Conn <nconn34@gmail.com>
2022-04-10 11:06:16 -0700

    Updated Treat controller

--- SweetandSavory/Models/Item.cs
+++ SweetandSavory/Models/Item.cs
@@ -1,17 +1,17 @@
 using System.Collections.Generic;
 
-namespace ToDoList.Models
+namespace SweetandSavory.Models
 {
-    public class Item
+    public class Treat
     {
-        public Item()
+        public Treat()
         {
-            this.JoinEntities = new HashSet<CategoryItem>();
+            this.JoinEntities = new HashSet<FlavorTreat>();
         }
 
-        public int ItemId { get; set; }
+        public int TreatId { get; set; }
         public string Description { get; set; }
         public virtual ApplicationUser User { get; set; } 
-        public virtual ICollection<CategoryItem> JoinEntities { get;}
+        public virtual ICollection<FlavorTreat> JoinEntities { get;}
     }
 }
